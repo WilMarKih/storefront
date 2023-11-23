9f594f4e5dd923e4dc44a79e80c07f7d0b3447d6
WilMarKih <mjulius6192001@gmail.com>
2023-11-23 19:15:20 +0300

    changes

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x ,y = 1, 2
+    return x
+
 def say_hello(request):
+    x = calculate()
     return render(request, 'hello.html', {'name':'Wilson'})
